4ad1fbbcb9ff8cf5738c0c825ed3fe1f6b920c67
unknown <megalit2578@gmail.com>
2025-09-02 23:13:20 +0700

    merge code

--- SuperChip/obj/Debug/net8.0-windows/SuperChip.AssemblyInfo.cs
+++ SuperChip/obj/Debug/net8.0-windows/SuperChip.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperChip")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb20361640b45295eb7ae3688170df23ddb61b2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperChip")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperChip")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
